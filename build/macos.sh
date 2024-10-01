84fdddff8abb7fbe765451751d141dfb5e83086f
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-01 20:42:12 +0530

    Attempt switching to `pkg-config` for `pcre` on mac

--- build/macos.sh
+++ build/macos.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-CFLAGS="-I./include -O3 -I$(brew --prefix pcre)/include"
-LIBS="-L$(brew --prefix pcre)/lib -lm -ldl -lpcre"
+pkg-config --list-all | grep pcre # Temporary
+# CFLAGS="-I./include -O3 -I$(pkg-config -- pcre)/include"
+# LIBS="-L$(brew --prefix pcre)/lib -lm -ldl -lpcre"
 
-rm -rf bin lib
-mkdir -p bin lib/.build
+# rm -rf bin lib
+# mkdir -p bin lib/.build
 
-for file in $(ls src/bs); do
-    cc $CFLAGS -o lib/.build/$file.o -c -fPIC src/bs/$file
-done
+# for file in $(ls src/bs); do
+#     cc $CFLAGS -o lib/.build/$file.o -c -fPIC src/bs/$file
+# done
 
-cc $CFLAGS -o bin/bs src/bs.c lib/.build/* $LIBS
-cc $CFLAGS -o lib/libbs.dylib -shared lib/.build/* $LIBS
-ar rcs lib/libbs.a lib/.build/*
+# cc $CFLAGS -o bin/bs src/bs.c lib/.build/* $LIBS
+# cc $CFLAGS -o lib/libbs.dylib -shared lib/.build/* $LIBS
+# ar rcs lib/libbs.a lib/.build/*
