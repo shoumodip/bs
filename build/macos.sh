2983ab89a7b2c1d09b1e3fc3ae70262726035fb9
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-01 21:20:27 +0530

    Try to fix mac build

--- build/macos.sh
+++ build/macos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CFLAGS="-I./include -O3"
+CFLAGS="-I./include -O3 $(pkg-config --cflags libpcre2-8)"
 LIBS="-lm -ldl -lpcre2-8"
 
 rm -rf bin lib
@@ -10,6 +10,6 @@ for file in $(ls src/bs); do
     cc $CFLAGS -o lib/.build/$file.o -c -fPIC src/bs/$file
 done
 
-cc $CFLAGS -o bin/bs src/bs.c lib/.build/* $LIBS
+cc -v $CFLAGS -o bin/bs src/bs.c lib/.build/* $LIBS
 cc $CFLAGS -o lib/libbs.dylib -shared lib/.build/* $LIBS
 ar rcs lib/libbs.a lib/.build/*
