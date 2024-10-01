42ea11322bd19706c4c4622187826a9b6bb1b706
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-01 20:49:04 +0530

    Try to trace position of failed linking in mac

--- build/macos.sh
+++ build/macos.sh
@@ -3,9 +3,6 @@
 CFLAGS="-I./include -O3 $(pkg-config --cflags libpcre2-8)"
 LIBS="$(pkg-config --cflags libpcre2-8) -lm -ldl -lpcre"
 
-echo "CFLAGS = $CFLAGS"
-echo "LIBS = $LIBS"
-
 rm -rf bin lib
 mkdir -p bin lib/.build
 
@@ -13,6 +10,7 @@ for file in $(ls src/bs); do
     cc $CFLAGS -o lib/.build/$file.o -c -fPIC src/bs/$file
 done
 
-cc $CFLAGS -o bin/bs src/bs.c lib/.build/* $LIBS
+echo "Building main executable"
+cc -v $CFLAGS -o bin/bs src/bs.c lib/.build/* $LIBS
 cc $CFLAGS -o lib/libbs.dylib -shared lib/.build/* $LIBS
 ar rcs lib/libbs.a lib/.build/*
