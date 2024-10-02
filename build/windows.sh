d9d8b46190fec9eb7b4367427e4d6dc1c90b531c
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-02 10:28:45 +0530

    Windows CI/CD is a pain. Try to cross compile from Linux

--- build/windows.sh
+++ build/windows.sh
@@ -2,21 +2,20 @@
 
 set -xe
 
-# Will this even work lmao? ChatGPT says it can. Oh well...
-pkg-config --list-all
+CC=x86_64-w64-mingw32-gcc
+AR=x86_64-w64-mingw32-ar
 
-CFLAGS="$(pkg-config --cflags libpcre) -I./include -O3"
-LIBS="-lm -ldl $(pkg-config --libs libpcre)"
+CFLAGS="-I./include -O3"
+LIBS="-lm -ldl -lpcre2-8"
 
 rm -rf bin lib
 mkdir -p bin lib/.build
 
 for file in $(ls src/bs); do
-    gcc $CFLAGS -o lib/.build/$file.o -c src/bs/$file
+    $CC $CFLAGS -o lib/.build/$file.o -c src/bs/$file
 done
 
-gcc $CFLAGS -o bin/bs.exe src/bs.c lib/.build/* $LIBS
-gcc $CFLAGS -o lib/libbs.dll -shared lib/.build/* $LIBS
+$CC $CFLAGS -o bin/bs.exe src/bs.c lib/.build/* $LIBS
+$CC $CFLAGS -o lib/libbs.dll -shared lib/.build/* $LIBS
 
-# Again, will this even work?
-ar rcs lib/libbs.lib lib/.build/*
+$AR rcs lib/libbs.lib lib/.build/*
