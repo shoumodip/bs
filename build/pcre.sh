8a6144fd8ea83e97d7f9e986c041117a5a42c580
Shoumodip Kar <shoumodipkar@gmail.com>
2024-10-03 14:42:42 +0530

    One stackoverflow post said MinGW is installed on github windows CI

--- build/pcre.sh
+++ build/pcre.sh
@@ -5,14 +5,7 @@ PKG=pcre2-10.44
 mkdir -p thirdparty && cd thirdparty
 
 if [ ! -d "$PKG" ]; then
-    URL="https://github.com/PCRE2Project/pcre2/releases/download/$PKG/$PKG.tar.bz2"
-
-    if [ "$1" = "win32" ]; then
-        powershell -Command "Invoke-WebRequest -Uri '$URL' -OutFile 'file.zip'"
-    else
-        wget "$URL"
-    fi
-
+    curl -LO "https://github.com/PCRE2Project/pcre2/releases/download/$PKG/$PKG.tar.bz2"
     tar fjx "$PKG.tar.bz2"
     rm -rf "$PKG.tar.bz2"
 fi
